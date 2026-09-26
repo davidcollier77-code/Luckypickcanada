39c459de2d02f301dcb1de10b474afe37f073941
davidcollier77-code <davidcollier77@gmail.com>
2026-09-26 03:42:23 -0300

    Update tailwind.config.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     extend: {
       fontFamily: {
         serif: ['"Playfair Display"', 'Georgia', 'serif'],
+        playfair: ['"Playfair Display"', 'Georgia', 'serif'],
         sans: ['Inter', 'sans-serif'],
       },
       dropShadow: {
